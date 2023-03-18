def6ffd9e3418c6a80de66306482859a0b427ecf
mrname <justincrown1@gmail.com>
2023-03-18 17:23:13 -0400

    Get django filter in the mix

--- scans/views.py
+++ scans/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
 
 from .models import Scan
 from .serializers import ScanSerializer
 
+class ScanFilter(filters.FilterSet):
+    class Meta:
+        model = Scan
+        fields = {
+            'hostname': ['exact'],
+            'result__total_load_time': ['lte', 'gte'],
+        }
+
 class ScanViewSet(viewsets.ModelViewSet):
     queryset = Scan.objects.all().order_by('-created')
     serializer_class = ScanSerializer
+    filterset_class = ScanFilter
